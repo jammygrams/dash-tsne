67cac54a28fdb33a1c6e56450034945b641f7382
Xing Han Lu <xhlperso@gmail.com>
2018-06-15 12:11:55 -0400

    Changed Position to fixed

--- app.py
+++ app.py
@@ -38,7 +38,8 @@ external_css = [
     "https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css",
     "//fonts.googleapis.com/css?family=Raleway:400,300,600",
     "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
-    "https://codepen.io/chriddyp/pen/brPBPO.css",
+    # "https://codepen.io/chriddyp/pen/brPBPO.css",
+    "https://rawgit.com/plotly/dash-tsne/master/custom_styles.css",
     "https://cdn.rawgit.com/plotly/dash-app-stylesheets/2cc54b8c03f4126569a3440aae611bbef1d7a5dd/stylesheet.css"
 ]
 
