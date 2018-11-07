ceee39eed69285c524c06593e73c89b8d45d32c5
bcdunbar <branden.dunbar@gmail.com>
2018-11-07 14:38:49 +1100

    :scissors: rawgit

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ server = app.server
 
 if 'DYNO' in os.environ:
     app.scripts.append_script({
-        'external_url': 'https://cdn.rawgit.com/chriddyp/ca0d8f02a1659981a0ea7f013a378bbd/raw/e79f3f789517deec58f41251f7dbb6bee72c44ab/plotly_ga.js'
+        'external_url': 'https://codepen.io/plotly/pen/BGyZNa.js'
     })
 
     demo_mode = True
@@ -37,9 +37,7 @@ external_css = [
     "https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css",
     "https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css",
     "//fonts.googleapis.com/css?family=Raleway:400,300,600",
-    "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
-    "https://cdn.rawgit.com/plotly/dash-tsne/master/custom_styles.css",
-    "https://cdn.rawgit.com/plotly/dash-app-stylesheets/2cc54b8c03f4126569a3440aae611bbef1d7a5dd/stylesheet.css"
+    "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
 ]
 
 for css in external_css:
